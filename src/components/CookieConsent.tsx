bdc8102b9d0b5329b1be1eaf59d81c85ecc37d0c
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-10-10 22:34:34 +0200

    feat(cookie-consent): add wording

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -6,8 +6,22 @@ export interface CookieConsentOwnProps {
 
 export const CookieConsent: FC<CookieConsentOwnProps> = () => {
   return (
-    <nav className="navbar fixed-bottom bg-body-tertiary">
-      <div className="container-fluid">Cookie Consent banner</div>
+    <nav className="navbar fixed-bottom bg-body-tertiary w-100">
+      <div className="d-flex flex-column align-items-center">
+        <p className="px-4">
+          Nous utilisons les cookies de Google Analytics pour générer des données statistiques
+          anonymes sur votre façon d'utiliser le site. En acceptant, vous nous aider à améliorer ce
+          site.
+        </p>
+        <div className="d-flex flex-row align-items-center">
+          <button type="button" className="btn btn-success px-5">
+            Accepter
+          </button>
+          <button type="button" className="btn btn-secondary ms-4">
+            Refuser
+          </button>
+        </div>
+      </div>
     </nav>
   );
 };
