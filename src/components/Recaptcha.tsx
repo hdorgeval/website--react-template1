5cba73ee73cabe1930d8943d773ce84ab3358060
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-10-19 17:06:38 +0200

    fix(grecaptcha): try fix crash inside component

--- src/components/Recaptcha.tsx
+++ src/components/Recaptcha.tsx
@@ -30,74 +30,86 @@ export const Recaptcha: FC<RecaptchaOwnProps> = ({ theme, invalidFeedbackClassNa
     }
   }, []);
   useEffect(() => {
-    if (grecaptcha && typeof captchaId === 'number') {
-      return () => {
-        try {
-          if (grecaptcha && typeof captchaId === 'number') {
-            grecaptcha.reset(captchaId);
-            setRecaptchaResponse('');
+    try {
+      if (grecaptcha && typeof captchaId === 'number') {
+        return () => {
+          try {
+            if (grecaptcha && typeof captchaId === 'number') {
+              grecaptcha.reset(captchaId);
+              setRecaptchaResponse('');
+            }
+          } catch (error) {
+            /* empty */
           }
-        } catch (error) {
-          /* empty */
-        }
-      };
+        };
+      }
+    } catch (error) {
+      /* empty */
     }
   }, [captchaId]);
 
   useEffect(() => {
-    if (!hasLoadedRecaptchaApi) {
-      setTimeout(() => {
-        try {
-          if (grecaptcha) {
-            setHasLoadedRecaptchaApi(true);
+    try {
+      if (!hasLoadedRecaptchaApi) {
+        setTimeout(() => {
+          try {
+            if (grecaptcha) {
+              setHasLoadedRecaptchaApi(true);
+            }
+          } catch (error) {
+            /* empty */
           }
-        } catch (error) {
-          /* empty */
-        }
-      }, 3000);
+        }, 3000);
+      }
+    } catch (error) {
+      /* empty */
     }
   }, [hasLoadedRecaptchaApi]);
 
   useEffect(() => {
-    if (!hasLoadedRecaptchaApi) {
-      return;
-    }
-
-    const captchaContainer = document.querySelector(
-      'div[data-netlify-recaptcha="true"]',
-    ) as HTMLDivElement;
-
-    const hasRendered = captchaContainer?.hasChildNodes() ?? false;
-    if (hasRendered) {
-      return;
-    }
-
-    if (captchaContainer && grecaptcha) {
-      try {
-        const captchaId = grecaptcha.render(captchaContainer, {
-          sitekey: websiteConfig.recaptchaV2.sitekey,
-          callback: captchaCallback,
-          'expired-callback': expiredCaptchaCallback,
-          theme: theme ?? websiteConfig.recaptchaV2.theme,
-          size: websiteConfig.recaptchaV2.size,
-        });
-        setCaptchaId(captchaId);
-
-        setTimeout(() => {
-          const captchaIframe = captchaContainer.querySelector('iframe[title="reCAPTCHA"]');
-          if (captchaIframe) {
-            const iframeWitdh = captchaIframe.getAttribute('width');
-            const iframeHeight = captchaIframe.getAttribute('height');
-            if (iframeWitdh && iframeHeight) {
-              captchaIframe.setAttribute('width', `${Number(iframeWitdh) - 3}`);
-              captchaIframe.setAttribute('height', `${Number(iframeHeight) - 3}`);
-              captchaIframe.setAttribute('class', `rounded-3 border border-1`);
-            }
-          }
-        }, 0);
-      } catch (error) {
-        /* empty */
+    try {
+      if (!hasLoadedRecaptchaApi) {
+        return;
       }
+
+      const captchaContainer = document.querySelector(
+        'div[data-netlify-recaptcha="true"]',
+      ) as HTMLDivElement;
+
+      const hasRendered = captchaContainer?.hasChildNodes() ?? false;
+      if (hasRendered) {
+        return;
+      }
+
+      if (captchaContainer && grecaptcha) {
+        try {
+          const captchaId = grecaptcha.render(captchaContainer, {
+            sitekey: websiteConfig.recaptchaV2.sitekey,
+            callback: captchaCallback,
+            'expired-callback': expiredCaptchaCallback,
+            theme: theme ?? websiteConfig.recaptchaV2.theme,
+            size: websiteConfig.recaptchaV2.size,
+          });
+          setCaptchaId(captchaId);
+
+          setTimeout(() => {
+            const captchaIframe = captchaContainer.querySelector('iframe[title="reCAPTCHA"]');
+            if (captchaIframe) {
+              const iframeWitdh = captchaIframe.getAttribute('width');
+              const iframeHeight = captchaIframe.getAttribute('height');
+              if (iframeWitdh && iframeHeight) {
+                captchaIframe.setAttribute('width', `${Number(iframeWitdh) - 3}`);
+                captchaIframe.setAttribute('height', `${Number(iframeHeight) - 3}`);
+                captchaIframe.setAttribute('class', `rounded-3 border border-1`);
+              }
+            }
+          }, 0);
+        } catch (error) {
+          /* empty */
+        }
+      }
+    } catch (error) {
+      /* empty */
     }
   }, [captchaCallback, expiredCaptchaCallback, hasLoadedRecaptchaApi, theme]);
 
